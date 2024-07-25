431bbda73ab4022dd580dc86c9b1792dfc5e1025
cat <ktfeng29160@gmail.com>
2024-07-25 20:06:10 +0800

    import discord from discord.ext import commands
    client=commands.Bot(command_prefix=">",intents=discord.Intents.all())
    
    @client.event
    async def on_ready():
        print("Success: Bot is connected to Discord")
    
    @client.command()
    async def 嗨(ctx):
        await ctx.send("よっ")
    
    client.run("MTI2NTkyMjIxODc4NjAyOTYwOA.GPOcUG.22lGnV8oYt9uxdh27ojraaWNciTZo1pIKPB7XE")

---
+++ あきとロボ.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+
+client=commands.Bot(command_prefix=">",intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print("Success: Bot is connected to Discord")
+
+@client.command()
+async def 嗨(ctx):
+    await ctx.send("よっ")
+
+client.run("MTI2NTkyMjIxODc4NjAyOTYwOA.GPOcUG.22lGnV8oYt9uxdh27ojraaWNciTZo1pIKPB7XE")

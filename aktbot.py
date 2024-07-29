4461f5de3799c5cbfbf15a5997ade3aec166a30a
cat <ktfeng29160@gmail.com>
2024-07-29 22:40:44 +0800

    import discord from discord import app_commands from discord.ext import commands,tasks import os import asyncio from itertools import cycle
    bot=commands.Bot(command_prefix="%",intents=discord.Intents.all())
    
    @bot.event
    async def on_ready():
        print("Bot ready")
        activity = discord.Streaming(name="ULTRA C",url="https://www.youtube.com/watch?v=7WryveKlyX8")
        await bot.change_presence(status = discord.Status.idle, activity = activity)
        try:
            synced=await bot.tree.sync()
            print(f"Synced {len(synced)} commands")
        except Exception as e:
            print(e)
    
    @bot.tree.command(name="哈囉",description="讓彰人跟你說嗨")
    async def hello(interaction:discord.Interaction):
        await interaction.response.send_message(f"よっ")
    
    @bot.tree.command(name="ping",description="檢視機器人的延遲")
    async def ping(interaction:discord.Interaction):
        ping_embed=discord.Embed(title="Ping",color=discord.Color.orange())
        ping_embed.add_field(name=f"{bot.user.name}的延遲：",value=f"{round(bot.latency*1000)}ms",inline=False)
        await interaction.response.send_message(embed=ping_embed,ephemeral=True)
    
    async def load():
        for filename in os.listdir("./cogs"):
            if filename.endswith(".py"):
                await bot.load_extension(f"cogs.{filename[:-3]}")
    
    async def main():
        async with bot:
            await load()
            await bot.start("MTI2NTkyMjIxODc4NjAyOTYwOA.GPOcUG.22lGnV8oYt9uxdh27ojraaWNciTZo1pIKPB7XE")
    
    asyncio.run(main())

--- aktbot.py
+++ aktbot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import app_commands
 from discord.ext import commands,tasks
 import os
 import asyncio
@@ -11,15 +12,27 @@ async def on_ready():
     print("Bot ready")
     activity = discord.Streaming(name="ULTRA C",url="https://www.youtube.com/watch?v=7WryveKlyX8")
     await bot.change_presence(status = discord.Status.idle, activity = activity)
+    try:
+        synced=await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
+
+@bot.tree.command(name="哈囉",description="讓彰人跟你說嗨")
+async def hello(interaction:discord.Interaction):
+    await interaction.response.send_message(f"よっ")
+
+@bot.tree.command(name="ping",description="檢視機器人的延遲")
+async def ping(interaction:discord.Interaction):
+    ping_embed=discord.Embed(title="Ping",color=discord.Color.orange())
+    ping_embed.add_field(name=f"{bot.user.name}的延遲：",value=f"{round(bot.latency*1000)}ms",inline=False)
+    await interaction.response.send_message(embed=ping_embed,ephemeral=True)
+
 
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
-
-@bot.command(aliases=("hello","嗨","哈囉"))
-async def hi(ctx):
-    await ctx.send("よっ")
 
 async def main():
     async with bot:
